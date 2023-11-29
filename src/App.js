47e6dd4bdb0a3e309dbc03165f007e1c107218ac
Anthony-Quito <anthonystark25abril@gmail.com>
2023-11-29 16:44:25 -0500

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useEffect,useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import TableCoins from './components/TableCoins';
+
+function App() {
+
+  const[coins,setCoins] = useState([])
+
+  const getData = async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    console.log(res.data)
+    setCoins(res.data)
+  }
+
+
+  useEffect(() => { 
+    getData()
+  }, [])
+
+
+  return (
+    <div className="container">
+      <div className='row'>
+       
+      </div>
+      
+      <TableCoins coins={coins}/>
+    </div>
+  );
+}
+
+export default App;
